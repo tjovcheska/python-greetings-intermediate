4811e1506a44e782117f2596e9f1f3b32dd0fce8
Martins Tararujs <martins@Martinss-MacBook-Pro-2.local>
2023-07-22 00:22:24 +0300

    Update ap to use redis as cache

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify
+import redis
 
 app = Flask(__name__)
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+
+def get_greetings_from_redis():
+    cached_greeting = redis_client.get('latest_greeting')
+    if cached_greeting:
+        return cached_greeting
+    else:
+        # If the greeting is not cached in Redis, fetch it and cache it
+        greeting = {"greeting": "Hello from Python App!"}
+        redis_client.set('latest_greeting', greeting["greeting"])
+        return greeting["greeting"]
 
 @app.route('/greetings')
 def greetings():
-    return jsonify({"greeting": "Hello from Python App!"})
+    greeting = get_greetings_from_redis()
+    return jsonify({"greeting": greeting})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port="3000")
+    app.run(host="0.0.0.0", debug=True, port=3000)
